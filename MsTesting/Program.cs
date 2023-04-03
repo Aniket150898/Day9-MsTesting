b3ec78067215a063e8a1deee362e62449f6c8675
Aniket150898 <103835629+Aniket150898@users.noreply.github.com>
2023-04-03 19:34:04 +0530

    [Aniket]- Print welcome message

---
+++ MsTesting/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MsTesting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome To MsTesting program");
+        }
+    }
+}
